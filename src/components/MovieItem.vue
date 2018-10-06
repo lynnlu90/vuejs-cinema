b9fd8b3b2f66367cfab942f2f5a1c86f658acb76
Yuan <jsmania@163.com>
2018-10-06 22:28:42 +0800

    Completed detail page

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,25 @@
+<template id="">
+  <div class="movie">
+    <div class="movie-col-left">
+      <router-link v-bind:to="{name:'movie',params:{id:movie.imdbID}}">
+        <img v-bind:src='movie.Poster' alt="">
+      </router-link>
+    </div>
+    <div class="movie-col-right">
+      <div class="movie-title">
+        <router-link v-bind:to="{name:'movie',params:{id:movie.imdbID}}">
+          <h2>{{movie.Title}}</h2>
+        </router-link>
+        <span class='movie-rating'>{{movie.Rated}}</span>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+
+  export default {
+    props: ['movie'],
+  }
+</script>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,34 +1,75 @@
 <template id="">
   <div id='movie-list'>
-    <div v-for='movie in filteredMovies' class='movie'>
-      {{movie.title}}
+    <div v-if='filteredMovies.length' class="">
+      <movie-item v-for='movie in filteredMovies' v-bind:movie='movie.movie'>
+        <div class="movie-sessions">
+          <div v-for='session in filteredSessions(movie.sessions)' class="session-time-wrapper">
+            <div class="session-time">
+              {{formatSessionTime(session.time)}}
+            </div>
+          </div>
+        </div>
+      </movie-item>
     </div>
+    <div v-else-if='movies.length' class="no-results">
+      {{noResults}}
+    </div>
+    <div v-else class="no-results">
+      Loading...
+    </div>
+
   </div>
 </template>
 
 <script type="text/javascript">
   import genres from '../util/genres';
+  import times from '../util/times';
+  import MovieItem from './MovieItem.vue';
   export default {
-    data: function() {
-      return {
-        movies: [
-          {title:'Kill Bill',genre:genres.CRIME},
-          {title:'Home Alone',genre:genres.COMEDY},
-          {title:'Auston Powers',genre:genres.COMEDY}
-        ]
-      }
-    },
-    props: ['genre', 'time'],
+
+    props: ['genre', 'time', 'movies', 'day'],
     methods: {
+      formatSessionTime(raw) {
+        return this.$moment(raw).format('h:mm A');
+      },
+      filteredSessions(sessions) {
+        return sessions.filter(this.sessionPassesTimeFilter);
+      },
+      sessionPassesTimeFilter(session) {
+        if (!this.day.isSame(this.$moment(session.time),'day')) return false;
+        else if (this.time.length === 0 || this.time.length === 2) return true;
+        else if (this.time[0] === times.AFTER_6PM) return this.$moment(session.time).hour() >= 18;
+        else return this.$moment(session.time).hour() < 18;
+      },
       moviePassesGenreFilter(movie) {
         if (!this.genre.length) return true;
-        else return this.genre.find(genre => movie.genre === genre);
-      }
+        else {
+          let movieGenres = movie.movie.Genre.split(', ');
+          let matched = true;
+          this.genre.forEach(genre => {
+            if (movieGenres.indexOf(genre) === -1) matched = false;
+            else {
+
+            }
+          });
+          return matched;
+        }
+      },
     },
     computed: {
       filteredMovies() {
-        return this.movies.filter(this.moviePassesGenreFilter);
+        return this.movies
+              .filter(this.moviePassesGenreFilter)
+              .filter(movie => movie.sessions.find(this.sessionPassesTimeFilter));
+      },
+      noResults() {
+        let times = this.time.join(', ');
+        let genres = this.genre.join(', ');
+        return `No results for ${times}${times.length && genres.length ? ', ' : ''} ${genres}.`;
       }
+    },
+    components: {
+      MovieItem
     }
   }
 </script>
