2b32d69e0b51efd81ff74ccf7de7773809207fb5
Yuan <jsmania@163.com>
2018-10-07 23:20:24 +0800

    Completed day selector

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,5 +1,6 @@
 <template id="">
   <div class="" id='overview'>
+    <day-select v-bind:selected='day'></day-select>
     <div class="" id='main'>
       <movie-list v-bind:genre='genre' v-bind:time='time' v-bind:movies='movies' v-bind:day='day'></movie-list>
       <movie-filter></movie-filter>
@@ -10,11 +11,13 @@
 <script type="text/javascript">
 import MovieList from './MovieList.vue';
 import MovieFilter from './MovieFilter.vue';
+import DaySelect from './DaySelect.vue';
   export default {
     props: ['genre', 'time', 'movies', 'day'],
     components: {
       MovieList,
-      MovieFilter
+      MovieFilter,
+      DaySelect
     },
   }
 </script>
